82b818336920f14a8b81fb60107f148c6af922fd
Juadeb <juadebgabriel@gmail.com>
2023-09-22 13:55:07 +0100

    fix

---
+++ script.js
@@ -0,0 +1,53 @@
+let navbar = document.querySelector(".navbar");
+
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+  searchForm.classList.remove("active");
+};
+
+let searchForm = document.querySelector(".search-form");
+
+document.querySelector("#search-btn").onclick = () => {
+  searchForm.classList.toggle("active");
+  navbar.classList.remove("active");
+  cartItem.classList.remove("active");
+};
+
+let cartItem = document.querySelector(".cart-items-container");
+
+document.querySelectorAll("#cart-btn, #cart-btn2").onclick = () => {
+  cartItem.classList.toggle("active");
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
+};
+
+window.onscroll = () => {
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
+  cartItem.classList.remove("active");
+};
+
+// SROLL TO TOP
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+// SERVICES PAGINATION
